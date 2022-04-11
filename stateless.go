60a2b0e9c1a09e742e70a54cf2ab217299b75a71
Ralf Jung <research@ralfj.de>
2022-04-11 17:00:57 -0400

    stateless: factor new_cap computation into helper

--- stateless.go
+++ stateless.go
@@ -5,6 +5,15 @@ import (
 	"github.com/tchajed/goose/machine"
 )
 
+func compute_new_cap(old_cap uint64, min_cap uint64) uint64 {
+	var new_cap = old_cap * 2
+	if new_cap < min_cap {
+		// Guard against overflow, and other nonsense.
+		new_cap = min_cap
+	}
+	return new_cap
+}
+
 // Grow a slice to have at least `additional` unused bytes in the capacity.
 // Runtime-check against overflow.
 func reserve(b []byte, additional uint64) []byte {
@@ -12,15 +21,7 @@ func reserve(b []byte, additional uint64) []byte {
 	if uint64(cap(b)) < min_cap {
 		// Amortized allocation strategy: grow slice by at least a certain factor.
 		// Rust RawVec uses a factor of 2 so we follow that.
-		var new_cap = uint64(cap(b)) * 2
-		if new_cap < 8 {
-			// Too small allocations aren't worth it, grow to 8 at least.
-			new_cap = 8
-		}
-		if new_cap < min_cap {
-			// Guard against overflow, cap(b)==0, and other nonsense.
-			new_cap = min_cap
-		}
+		new_cap := compute_new_cap(uint64(cap(b)), min_cap)
 		// We make a new slice with length 0 and the desired capacity.
 		// Then we append `b` to that, which copies its elements without further allocation.
 		return append(make([]byte, 0, new_cap), b...)
